95e94b1dac033b2b597e79c9d93e4daaf7b2c151
WarisSripatoomrak <104860050+WarisSripatoomrak@users.noreply.github.com>
2022-05-07 19:37:19 +0700

    Add files via upload

---
+++ IoTFinalFinal (ของจริง)/IoTFinalFinal WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  IoTFinalFinal WatchKit Extension
+//
+//  Created by Waris Sripatoomrak on 5/7/2564 BE.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
